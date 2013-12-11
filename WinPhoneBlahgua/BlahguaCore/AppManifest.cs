2ee272c9cf6f5ca6108d75f7a5ac9564b8545700
Dave Vromay <davevr@eweware.com>
2013-12-10 21:19:07 -0800

    reverting to VS 2012

--- WinPhoneBlahgua/BlahguaCore/AppManifest.cs
+++ WinPhoneBlahgua/BlahguaCore/AppManifest.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Collections.Generic;
 using System.Xml.Linq;
 using System.ComponentModel;
 using System.Reflection;
--- WinPhoneBlahgua/BlahguaCore/GoogleAnalytics.cs
+++ WinPhoneBlahgua/BlahguaCore/GoogleAnalytics.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using RestSharp;
 using Microsoft.Phone.Info;
 using System.Net;
